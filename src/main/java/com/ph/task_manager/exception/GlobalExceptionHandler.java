fc2e521b1392b7c2655293cabf0293809875c869
Pavel Horishny <horishnypavel@gmail.com>
2026-07-07 19:54:50 +0200

    added global error handler

---
+++ src/main/java/com/ph/task_manager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ph.task_manager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Map<String,Object>> handleTaskNotFound(TaskNotFoundException e) {
+        Map<String,Object> body = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", HttpStatus.NOT_FOUND.value(),
+                "error", "Task not found",
+                "message", e.getMessage()
+        );
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
