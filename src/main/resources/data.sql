1845fc3ee4fd9b12ead788a09232995964883127
Pavel Horishny <horishnypavel@gmail.com>
2026-07-07 22:21:30 +0200

    add data.sql script for automatic test data initialization update yaml for correct work of preloading data on app start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tasks (title, description, done, created_at)
+VALUES ('Visit grocery', 'buy bread and milk', false, NOW());
+
+INSERT INTO tasks (title, description, done, created_at)
+VALUES ('Take new book', 'read at least 10 pages', false, NOW());
+
+INSERT INTO tasks (title, description, done, created_at)
+VALUES ('pay the bills', 'gym and rent', true, NOW());
