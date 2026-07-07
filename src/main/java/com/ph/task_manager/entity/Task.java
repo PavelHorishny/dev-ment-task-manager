e057755cfe226baafabcf006053f2c30008ec705
Pavel Horishny <horishnypavel@gmail.com>
2026-07-07 16:26:05 +0200

    added Task entity and repo

---
+++ src/main/java/com/ph/task_manager/entity/Task.java
@@ -0,0 +1,31 @@
+package com.ph.task_manager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    @Column(nullable = false)
+    private boolean done = false;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/ph/task_manager/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.ph.task_manager.repository;
+
+import com.ph.task_manager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
